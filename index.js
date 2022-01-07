1929f380529e47581ff4aa87f236b52b740a891c
Brooke Strand <brooke.strand007@gmail.com>
2022-01-06 19:02:24 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+//DOM to remove main id
+document.getElementById("main").remove();
+
+//create h1 with id #victory
+const victor = document.createElement("h1")
+document.body.append(victor);
+//Make h1 say "Brooke is the champion"
+victor.setAttribute("id", "victory");
+victor.innerHTML = 
+"<h1>Brooke is the champion</h1>";
